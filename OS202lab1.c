78222068745b450a4ade43ddad0fa1a741a8729b
davidvalentine99 <60243651+davidvalentine99@users.noreply.github.com>
2020-10-26 22:41:00 -0400

    Update OS202lab1.c

--- OS202lab1.c
+++ OS202lab1.c
@@ -12,25 +12,21 @@ int main(int argc, char ** argv, char **envp){
         char input[20];
         scanf("%s", input);
         printf("Parent Process %d\n", getpid());
-        //printf("%s\n", input);
 
-        //printf("1\n");
+
         if (strcmp(input, "exit") == 0){
             exit(0);
         }
 
         else if (strcmp(input, "printid") == 0){
-            //printf("2\n");
             printf("The ID of the current process is %d\n", getpid());
         }
 
         else if (strcmp(input, "greet") == 0){
-            //printf("3\n");
             printf("Hello\n");
         }
-
+        
         else{
-            //printf("4\n");
             char bin[11] = "/bin/";
             strcat(bin, input);
             int child_status;
@@ -38,12 +34,10 @@ int main(int argc, char ** argv, char **envp){
 
             int pid = fork();
             if (pid == 0){
-                //printf("5\n");
 
                 printf("Child process %d will execute the command %s\n", getpid(), input);
 
                 execve(args[0], args, NULL);
-                //printf("6\n");
                 printf("Command Not Found!\n");
                 exit(0);
             }
