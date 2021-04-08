44c356a942cbd0295b05cbe9eca24c64ce5acfee
davidvalentine99 <60243651+davidvalentine99@users.noreply.github.com>
2021-04-08 14:11:16 -0400

    Update histogram.c

--- histogram.c
+++ histogram.c
@@ -8,7 +8,6 @@ int main(int argc, char *argv[]){
 int num_floats = 0;
 int num_bins = atoi(argv[1]);
 int num_threads = atoi(argv[2]);
-omp_set_num_threads(num_threads);
 //printf("Checkpoint 1a\n");
 
 FILE * fp; 
@@ -24,11 +23,11 @@ fscanf(fp, "%d", &num_floats);
 //printf("Checkpoint 1c\n");
 
 float* x = malloc(sizeof(float) * num_floats);
-int local_hist[num_threads+1][num_bins];
+int local_hist[num_threads][num_bins];
 int global_hist[num_bins];
 	
 for (int i = 0; i < num_bins; i++){
-	for (int j = 0; j <= num_threads; j++){
+	for (int j = 0; j < num_threads; j++){
 		local_hist[j][i] = 0;
 	}
 	global_hist[i] = 0;
@@ -55,14 +54,14 @@ for (int i = 0; i < num_bins; i++){
 printf("\n");
 
 //printf("Checkpoint 1\n");
-#pragma omp parallel shared(local_hist, global_hist)
+#pragma omp parallel num_threads(num_threads) shared(local_hist, global_hist)
 {
 	int tid = omp_get_thread_num();
 	#pragma omp for nowait
 	for (int i = 0; i < num_floats; i++){
 		for (int j = 1; j <= num_bins; j++){
 			if (x[i] < (bin_sz * j)){
-				local_hist[tid+1][(j-1)]++;
+				local_hist[tid][(j-1)]++;
 				break;
 			}
 		}
@@ -73,7 +72,7 @@ printf("\n");
 	if (tid == 0){
 		printf("\n");
 		for (int i = 0; i < num_bins; i++){
-			printf("bin[%d] = %d\n", i, local_hist[tid+1][i]);
+			printf("bin[%d] = %d\n", i, local_hist[tid][i]);
 		}
 	}
 	
@@ -81,7 +80,7 @@ printf("\n");
 
 	for (int i = 0; i < num_bins; i++){
 		#pragma omp atomic
-		global_hist[i] += local_hist[tid+1][i];	
+		global_hist[i] += local_hist[tid][i];	
 	}
 	//printf("Checkpoint 3\n");
 	/*
